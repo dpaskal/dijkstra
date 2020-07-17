d0e5eeb6f1cf55c91e14dd549ad0ad68ea545d65
Daniel Paskalev <dpaskal1@binghamton.edu>
2020-07-17 02:55:22 -0400

    first and second line of input file ready

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -4,15 +4,33 @@
 using namespace std;
 
 int main(int argc, char **argv) {
+	ifstream inFile;
+	int numNodes;
+
 	if (!(argc == 2)) {
 		cerr << "Usage: ./dijkstra <input.txt>" << endl;
 		exit(EXIT_FAILURE);
 	}
 
-	if (!ifstream(argv[1])) {
-		cerr << "error opening input.txt" << endl;
-		exit(EXIT_FAILURE);
+	// Open file
+	inFile.open(argv[1]);
+
+	// First line
+	inFile >> numNodes;
+	cout << "numNodes = " << numNodes << endl; // debug
+
+	// Second line
+	char nodeNames[numNodes];
+
+	for(int i=0; i < numNodes; i++) {
+		inFile >> nodeNames[i];
 	}
+	cout << "nodeNames = " << nodeNames << endl; // debug
+
+	// third line
+
+
+
 
 	return 0;
 }
