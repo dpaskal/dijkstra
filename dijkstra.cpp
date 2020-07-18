1560d9c6fcb8b4f7c564de8ecb9f2d14447d3bfa
Daniel Paskalev <dpaskal1@binghamton.edu>
2020-07-18 02:05:37 -0400

    prettyprint looking pretty

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -3,7 +3,28 @@
 
 using namespace std;
 
+// Helper function for pretty printing adjacency matrix
+void prettyPrint(int numNodes, char* nodeNames, int** matrix) {
+	char tab = '\t';
+	cout << "Adjacency matrix: " << endl;
+	cout << endl;
+	cout << tab;
+	for(int i=0; i<numNodes; ++i)
+		cout << nodeNames[i] << tab;
+	cout << endl << endl;
+
+	for(int i=0; i<numNodes; ++i) {
+		cout << nodeNames[i] << tab;
+		for(int j=0; j<numNodes; ++j) {
+			cout << matrix[i][j] << tab;
+		}
+		cout << endl << endl;
+
+	}
+}
+
 int main(int argc, char **argv) {
+	char startNode;
 	ifstream inFile;
 	int numNodes;
 
@@ -40,21 +61,34 @@ int main(int argc, char **argv) {
 	}
 
 	// Debug print the matrix
-	for(int i=0; i<numNodes; ++i) {
-		for(int j=0; j<numNodes; ++j) {
-			cout << matrix[i][j] << ' ';
-		}
-		cout << endl;
+	// for(int i=0; i<numNodes; ++i) {
+	// 	for(int j=0; j<numNodes; ++j) {
+	// 		cout << matrix[i][j] << ' ';
+	// 	}
+	// 	cout << endl;
+	// }
+	prettyPrint(numNodes, nodeNames, matrix);
+
+
+	// Read an input from the user regarding the starting node
+	cout << "Which of these nodes should we start at: ";
+	for(int i=0; i<numNodes; i++)
+		cout << nodeNames[i] << ' ';
+	cout << "? ";
+	cin >> startNode;
+
+	// Verify the input is valid
+	bool found = false;
+	for(int i=0; i<numNodes; i++)
+		if(startNode == nodeNames[i])
+			found = true;
+
+	if(!found) {
+		cerr << "input value is not a valid node" << endl;
+		exit(EXIT_FAILURE);
 	}
-
-
-	// Dijkstra's algorithm time
 
 	return 0;
 }
 
 
-// Helper function for pretty printing adjacency matrix
-void prettyPrint(int numNodes, char* nodeNames, int** matrix) {
-	;
-}
